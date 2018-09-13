1f523bdc9a6d6683c4e577a9646517a0b128e1a9
Andre Muramatsu <muramatsu@outlook.com>
2018-09-13 15:43:32 -0400

    Nu HTML Checker usage

---
+++ src/Pystola/libs/vnu.py
@@ -0,0 +1,25 @@
+# vim: set fileencoding=utf8 :
+
+import subprocess
+import os
+import tempfile
+import json
+
+class vnu():
+
+    def __init__(self, lib_dir):
+        self.__lib_dir = lib_dir
+
+    def validate_html(self, src):
+        tmpf = tempfile.NamedTemporaryFile()
+        tmpf.write(src.encode('utf-8'))
+        proc = subprocess.Popen(
+                ['java', '-jar', self.__lib_dir + '/vnu/vnu.jar', '--errors-only', '--format', 'json', tmpf.name],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        resp = proc.communicate()
+        ret = []
+        if resp[1] is not None:
+            ret = json.loads(resp[1].decode('utf-8'))
+        return ret
+
